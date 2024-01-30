90d03358e83a3af252474806d4b5eec3e251c2a5
sunpreetsandhey <147057463+sunpreetsandhey@users.noreply.github.com>
2024-01-30 11:00:31 +0000

    Add files via upload

---
+++ WeeklyExercises/1GettingStarted/Hello.java
@@ -0,0 +1,10 @@
+public class Hello
+{
+    //----------------------------------------------------
+    //  Prints a statement.
+    //----------------------------------------------------
+    public static void main (String[] args)
+    {
+        System.out.println ("Hello world");
+    }
+}
---
+++ WeeklyExercises/1GettingStarted/Test.java
@@ -0,0 +1,10 @@
+public class Test
+{
+    //----------------------------------------------------
+    //  Prints a statement.
+    //----------------------------------------------------
+    public static void main (String[] args)
+    {
+        System.out.println ("An Emergency Broadcast");
+    }
+}
