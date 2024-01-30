911c9391656000187573079b0dedd1b8aff83769
sunpreetsandhey <147057463+sunpreetsandhey@users.noreply.github.com>
2024-01-30 11:17:08 +0000

    Add files via upload

---
+++ WeeklyExercises/1GettingStarted/Personaldetails.java
@@ -0,0 +1,13 @@
+public class Personaldetails { public static void main(String[] args) {
+    // Personal details
+    String name = "Sunpreet sandhey";
+    String address = "47 elsemere avenue, Doncaster";
+    int age = 21;
+    String phoneNumber = "555-1234";
+
+    // Output personal details to the screen
+    System.out.println("Personal Details:");
+    System.out.println("Name: " + name);
+    System.out.println("Address: " + address);
+    System.out.println("Age: " + age);
+    System.out.println("Phone Number: " + phoneNumber); }}
