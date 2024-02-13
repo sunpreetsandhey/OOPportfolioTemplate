69ec036b237bdb81842962a9085c89ff7f84e847
sunpreetsandhey <147057463+sunpreetsandhey@users.noreply.github.com>
2024-02-13 10:36:57 +0000

    Add files via upload

---
+++ WeeklyExercises/2DataAndExpressions/Average.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Average
+{
+    public static void main(String[] args)
+    {
+        int val1, val2, val3;
+        double average;
+        Scanner scan = new Scanner(System.in) ;
+
+        System.out.println("Please enter three integers and " +
+                "I will compute their average");
+
+
+        System.out.println("Enter the first value:");
+        val1 =scan.nextInt();
+
+        System.out.println("Enter the second value:");
+        val2 = scan.nextInt();
+
+        System.out.println("Enter the third value:");
+        val3 =scan.nextInt();
+
+        average= val1 + val2 + val3 / 3.0;
+
+        System.out.println("The average is " + average + ".");
+
+
+
+
+    }
+}
